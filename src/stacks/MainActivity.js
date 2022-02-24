b97bd248d57034240b34fd41471eca75893074cd
AlannaSantos <alannacastro.ds@gmail.com>
2022-02-24 17:08:48 -0300

    atualização da tela do usuario.

--- src/stacks/MainActivity.js
+++ src/stacks/MainActivity.js
@@ -5,16 +5,38 @@ import MainTab from './MainTab';
 import SrunLogo from '../views/SrunLogo';
 import SignIn from '../views/SignIn';
 import SignUp from '../views/SignUp';
+import RegisterUser from "../views/RegisterUser/RegisterUser";
+import ListActivitySchedule from "../views/ListActivitySchedule/ListActivitySchedule";
 
 const Stack = createNativeStackNavigator();
 
 export default function MainActivity(){
     return(
-        <Stack.Navigator initialRouteName='SrunLogo' screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='SrunLogo' component={SrunLogo}/>
-            <Stack.Screen name='SignIn' component={SignIn}/>
-            <Stack.Screen name='SignUp' component={SignUp}/>
-            <Stack.Screen name='MainTab' component={MainTab}/>
-        </Stack.Navigator>
-    ) 
+            <Stack.Navigator initialRouteName='MainTab' screenOptions={{ headerShown: false }}>
+                <Stack.Screen name='SrunLogo' component={SrunLogo}/>
+                <Stack.Screen name='SignIn' component={SignIn}/>
+                <Stack.Screen name='SignUp' component={SignUp}/>
+                <Stack.Screen name='MainTab' component={MainTab}/>
+                <Stack.Screen name='RegisterUser' component={RegisterUser}
+                    options={{
+                        headerShown:true,
+                        title:'Edição',
+                        headerTintColor:'#ffffff',
+                        headerStyle: {
+                            backgroundColor: '#48bfe3',
+                        },
+                        headerTitleAlign:'center',
+                    }}/>
+                <Stack.Screen name='ListActivitySchedule' component={ListActivitySchedule}
+                              options={{
+                                  headerShown:true,
+                                  title:'Lista Atividades Agendadas',
+                                  headerTintColor:'#ffffff',
+                                  headerStyle: {
+                                      backgroundColor: '#48bfe3',
+                                  },
+                                  headerTitleAlign:'center',
+                              }}/>
+            </Stack.Navigator>
+    )
 }
--- src/views/TopBarNav/UserProfile/UserProfile.js
+++ src/views/TopBarNav/UserProfile/UserProfile.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
-//import base64 from 'base-64'
 import AsyncStorageLib from '@react-native-async-storage/async-storage';
+import {useNavigation} from "@react-navigation/native";
 
-import Vieww from '../../../components/Vieww';
+export default function UserProfile({navigation}){
 
-export default function UserProfile(){
-    
     const [image, setImage] = React.useState(null);
     const [userData, setUserData] = React.useState(null);
-    const pickImage = async() => {
 
+    const pickImage = async() => {
         let result = await ImagePicker.launchImageLibraryAsync({
             base64: true,
             mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -19,14 +17,10 @@ export default function UserProfile(){
             aspect: [4, 3],
             quality: 1
         });
-
-        //console.log(result);
-
         if(!result.cancelled){
             setImage(result);
         }
     }
-
     React.useEffect(() => {
         const mostrarNome = async () => {
             const storaged = await AsyncStorageLib.getItem('@Srun:user');
@@ -34,80 +28,118 @@ export default function UserProfile(){
         }
         mostrarNome();
     }, []);
-    
-    //let usuario = JSON.parse(storageUser);
-    /*let imageUri = image ? `data:image/jpg;base64,${image.base64}` : null;
-    image && console.log("Tamanho com bits extra" + imageUri.length);
-
-    if(imageUri != null){
-        //let strencoded = base64.encode(image.base64);
-        //console.log("Tamanho encode: " + strencoded.length);
-        let strdecoded = base64.decode(image.base64);
-        console.log("Tamanho decode: " + strdecoded.length);
-    }*/
-
-    //let strencoded = base64
-    //
-    //imageUri && console.log(strdecoded.length);
+   // console.log(image);
+    //'https://as1.ftcdn.net/v2/jpg/01/40/46/18/1000_F_140461899_dvRngd7xvZtqCUHLiIyRjgflq2EmwnVP.jpg'
 
     return (
-        
-        <View style={css.container}>
-            <View style={header.view}>
-                <Text style={header.text}>PERFIL</Text>
-            </View>
-            <Vieww lbl="Nome" lblValue={userData && userData.firstname + " " + userData.lastname}/>
-            <Vieww lbl="Email" lblValue={userData && userData.email }/>
-            {/* <View style={css.view}>
-                <TouchableOpacity style={css.btnImage} onPress={pickImage}>
-                    {image && <Image source={{ uri: `data:image/jpg;base64,${image.base64}` }} style={{ width: 80, height: 80, borderRadius: 50 }} /> }
+        <>
+            <View style={css.container} >
+                <View style={header.view}>
+                    <Text style={header.text}>{ userData && userData.firstname +' '+ userData.lastname }</Text>
+                </View>
+                <TouchableOpacity onPress={pickImage} style={header.touchableOpacity}>
+                    <Text style={{color:'#808080'}}>Pick an Image</Text>
+                    {image && <Image
+                        source={{
+                           uri:`data:image/jpg;base64,${image.base64}`
+                        }} style={header.avatar} /> }
                 </TouchableOpacity>
-            </View> */}
-        </View>
+                <View style={header.textView}>
+                    <Text style={header.text}> E-mail: { userData && userData.email }</Text>
+                </View>
+                <View style={header.textView}>
+                    <Text style={header.text}> Gênero: { userData && ''}</Text>
+                </View>
+                <View style={header.textView}>
+                    <Text style={header.text}> Data de Nascimento: { userData && '' }</Text>
+                </View>
+                <View style={header.touchableOpacity}>
+                    <Text style={header.text}> Peso: { userData && '' }</Text>
+                </View>
+                <View style={header.touchableOpacity}>
+                    <Text style={header.text}> Altura: { userData && '' }</Text>
+                </View>
+            </View>
+                <TouchableOpacity style={footer.btnEdit} onPress={() => navigation.navigate("RegisterUser")} >
+                    <Text style={footer.lblBtnEdit}>EDITAR CONTA</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={footer.btnActivity} onPress={()=> navigation.navigate("ListActivitySchedule")}>
+                    <Text style={footer.lblBtnActivity}>MINHAS ATIVIDADES</Text>
+                </TouchableOpacity>
+        </>
     )
 }
 
 const css = StyleSheet.create({
     container: {
-        flex: 1,
-        //justifyContent: 'center',
-        alignItems: 'center'
+        flex:1,
+        flexDirection: 'row-reverse',
+        flexWrap: 'wrap',
     },
-    view:{
-        flex: 1,
-    },
-    //imgContainer: {
-        //padding: 2,
-        //flex: 1,
-        //flexDirection: 'row'
-        //alignItems: 'flex-start'
-        //left: 0
-        //flexDirection: 'row',
-        //alignItems: 'center'
-        //flex
-    //},
-    btnImage: {
-        marginTop: 5,
-        borderWidth: 1,
-        borderColor: '#FFF',
-        //padding: 10,
-        //backgroundColor: '#48bfe3',
-        width: 80, height: 80, borderRadius: 50 
-    }
 });
-
 const header = StyleSheet.create({
     view: {
-        width: '100%',
-        paddingTop: 12,
-        paddingBottom: 12,
-        borderBottomWidth: 1,
+        width: '50%',
+        paddingTop: 50,
+        borderBottomWidth: 2,
         borderColor: '#D3D3D3',
-        alignItems: 'center'
+
     },
     text: {
         color: '#808080',
         fontWeight: 'bold',
-        fontSize: 16
+        fontSize: 16,
+    },
+    touchableOpacity:{
+        width: '50%',
+        paddingTop: 12,
+        borderBottomWidth: 2,
+        borderColor: '#D3D3D3'
+
+    },
+    avatar:{
+       width:100,
+        height:100,
+        borderRadius: 50
+    },
+    textView:{
+        width:'100%',
+        paddingTop: 12,
+        borderBottomWidth: 2,
+        borderColor: '#D3D3D3',
+        flexWrap:'wrap'
+    }
+});
+
+const footer = StyleSheet.create({
+    lblBtnEdit: {
+        color: '#FFF',
+        fontWeight: 'bold',
+    },
+    btnEdit: {
+        width: '80%',
+        position: 'absolute',
+        bottom: 50,
+        marginBottom: 20,
+        padding: 10,
+        backgroundColor: '#069e67',
+        borderRadius: 6,
+        alignItems: "center",
+        marginLeft:50,
+    },
+    lblBtnActivity: {
+        color: '#FFF',
+        fontWeight: 'bold',
+    },
+    btnActivity: {
+        width: '80%',
+        position: 'absolute',
+        bottom: 0,
+        marginBottom: 20,
+        padding: 10,
+        backgroundColor: '#48bfe3',
+        borderRadius: 6,
+        alignItems: "center",
+        marginLeft:50
     }
 });
